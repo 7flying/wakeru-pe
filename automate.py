5d0205c452f4373b5ff395d0d5934429e477b7e1
7flying <diez.f.irene@gmail.com>
2016-01-12 23:22:33 +0100

    Add function to extract mnemonics

--- automate.py
+++ automate.py
@@ -2,7 +2,7 @@
 """
 Automates the dataset generation process.
 """
-import subprocess
+import subprocess, re
 from sys import argv
 from os import listdir
 from os.path import isfile, join
@@ -23,6 +23,31 @@ def disassemble(path_to_file):
         print " [ ERROR ] Error with sample %s" % (path_to_file)
     error_file.close()
     return out
+
+def get_mnemonics(path_to_file):
+    """Returns a dict with the opcode mnemonic and number of times it has been
+    used."""
+    regex = r'(\s|\t)+\d+:(\s|\t)+([a-f0-9][a-f0-9]\s)+(\s|\t)+([a-zA-Z0-9.]+)'
+    ret = {}
+    with open(path_to_file, 'r') as fil:
+        for line in fil:
+            result = re.match(regex, line)
+            if result:
+                code = result.group(5)
+                if ret.get(code) is None:
+                    ret[code] = 0
+                ret[code] += 1
+    return ret
+
+def get_mnemonics_folders(folder_list):
+    mnemonics = []
+    for folder in folder_list:
+        for sample in listdir(folder):
+            if isfile(join(folder_list, sample)):
+                ret = get_mnemonics(sample)
+                if len(ret.keys()):
+                    mnemonics.append(ret)
+    return mnemonics # TODO add a set with the unique mnemonics
 
 def main(dataset_name, malware_path, goodware_path, count_samples=50):
     """Disassembles PE files on malware_path and goodware_path folders,
